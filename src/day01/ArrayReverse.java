5ed9b02211b36001fab6134584239bbe1ef30da2
Steve Geluso <stevegeluso@gmail.com>
2018-07-12 15:41:40 -0700

    largest product solution

---
+++ src/day01/ArrayReverse.java
@@ -0,0 +1,7 @@
+package day01;
+
+public class ArrayReverse {
+    public static void reverseArray(int[] arr) {
+
+    }
+}
---
+++ src/day01/ArrayReverseTest.java
@@ -0,0 +1,12 @@
+package day01;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArrayReverseTest {
+    @Test
+    public void myTest() {
+        int[] numbers = {1, 2, 3};
+        assertEquals(numbers, numbers);
+    }
+}
